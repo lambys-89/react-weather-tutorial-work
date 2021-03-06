e1518fcfcf56bc279a60ebb78dca71fd976707ba
lambys-89 <75185120+lambys-89@users.noreply.github.com>
2021-03-06 22:58:37 +0000

    Basic page setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import background from "./media/01d-vid.mp4";
 import './App.css';
 
 function App() {
 
   return(
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 id="pageTitle">
+          Weather Forecast
+        </h1>
+      </header>
+      <main className="App-main">
+      <video id="background-video"
+        className="videoTag"
+        autoPlay
+        muted
+        loop
+        src={background}
+      />
+      </main>
+      <footer className="App-footer">
+        <p className="open-source-code">
+          <a className="App-link" href="https://github.com/lambys-89/react-weather-tutorial-work/">
+            Open-source code
+          </a>{" "}
+          by Sarah Lamb
+        </p>
+      </footer>
     </div>
   )
 
